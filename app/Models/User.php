bfac5f1fa98e9ae8934cbeb0285ed5a6747b5fe9
“yangwb1” <770260332@qq.com>
2018-11-29 08:15:37 +0000

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -99,4 +99,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
