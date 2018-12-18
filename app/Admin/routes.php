d6f87da850d26b121d6d35c4d0ec804d50b0318e
“yangwb1” <770260332@qq.com>
2018-12-18 02:54:19 +0000

    管理后台 - 新增、编辑商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -15,4 +15,10 @@ Route::group([
     $router->get('users', 'UsersController@index');
     //商品列表
     $router->get('products', 'ProductsController@index');
+    //创建商品
+    $router->get('products/create', 'ProductsController@create');
+    $router->post('products', 'ProductsController@store');
+    //编辑商品
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    $router->put('products/{id}', 'ProductsController@update');
 });
