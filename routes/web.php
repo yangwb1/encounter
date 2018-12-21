d118438dac5cd5cb2a972aba16ca1ef199b32e9a
“yangwb1” <770260332@qq.com>
2018-12-21 01:17:11 +0000

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -80,4 +80,7 @@ Route::post('cart', 'CartController@add')->name('cart.add');
 Route::get('cart', 'CartController@index')->name('cart.index');
 Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
+//订单
+Route::post('orders', 'OrdersController@store')->name('orders.store');
+
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
